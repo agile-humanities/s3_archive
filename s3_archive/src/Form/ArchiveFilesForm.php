2d09971eceaf11a21598973488ff1cc62226c676
ajstanley <alanjarlathstanley@gmail.com>
2024-07-09 20:29:02 -0300

    added drush and custom Action

---
+++ s3_archive/src/Form/ArchiveFilesForm.php
@@ -0,0 +1,262 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\s3_archive\Form;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\islandora\IslandoraUtils;
+use Drupal\s3fs\S3fsFileService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a S3 archive form.
+ */
+final class ArchiveFilesForm extends FormBase {
+
+  /**
+   * The S3 Bucket connection.
+   *
+   * @var \Drupal\s3fs\S3fsFileService
+   */
+  protected $s3fs;
+
+  /**
+   * The Islandora Utils service.
+   *
+   * @var \Drupal\islandora\IslandoraUtils
+   */
+  protected IslandoraUtils $utils;
+
+  /**
+   * Database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * Provides helpers to operate on files and stream wrappers.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
+   * Config settings.
+   *
+   * @var string
+   */
+  const SETTINGS = 's3_archive.settings';
+
+  /**
+   * Array of terms for nodes with children.
+   *
+   * @var array
+   */
+  protected $collection_terms;
+
+  /**
+   * Standard Constructor.
+   *
+   * @param \Drupal\s3fs\S3fsFileService $s3fs
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   * @param \Drupal\islandora\IslandoraUtils $utils
+   * @param \Drupal\Core\Database\Connection $database
+   *   *.
+   */
+  public function __construct(S3fsFileService $s3fs, EntityTypeManagerInterface $entity_type_manager, IslandoraUtils $utils, Connection $database) {
+    $this->s3fs = $s3fs;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->utils = $utils;
+    $this->database = $database;
+    $this->collection_terms = [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('s3fsfileservice'),
+      $container->get('entity_type.manager'),
+      $container->get('islandora.utils'),
+      $container->get('database'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 's3_archive_archive_files';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form['collection'] = [
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'node',
+      '#title' => $this->t('Collection'),
+      '#description' => $this->t('Select collection.'),
+      '#tags' => TRUE,
+      '#selection_settings' => [
+        'target_bundles' => ['islandora_object'],
+      ],
+      '#weight' => '0',
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+      'submit' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Archive Files'),
+      ],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $term_uris = [
+      'http://purl.org/dc/dcmitype/Collection',
+      'http://vocab.getty.edu/aat/300242735',
+      'https://schema.org/Newspaper',
+      'https://schema.org/Book',
+      'https://schema.org/PublicationIssue',
+    ];
+    foreach ($term_uris as $uri) {
+      $term = $this->utils->getTermForUri($uri);
+      $this->collection_terms[] = $term->id();
+    }
+    $collection = $form_state->getValue('collection');
+    $collection_nid = $collection[0]['target_id'];
+    $containers = [$collection_nid];
+    $subcollections = [$collection_nid];
+    while (count($subcollections) > 0) {
+      $subcollections = $this->getCollectors($subcollections);
+      $containers = array_merge($subcollections, $containers);
+    }
+    $results = $this->getResults($containers);
+    $operations = [];
+    foreach ($results as $result) {
+      $this->processResult($result);
+      $operations[] = [
+        [$this, 'processResult'],
+        [$result],
+      ];
+    }
+    $batch = [
+      'title' => $this->t("Archiving files..."),
+      'operations' => $operations,
+      'progress_message' => $this->t('Processed @current out of @total. Estimated time: @estimate.'),
+      'error_message' => $this->t('The process has encountered an error.'),
+    ];
+    batch_set($batch);
+  }
+
+  /**
+   * Gets results from database.
+   *
+   * @param array $collection_ids
+   *   Nids of all nodes with children.
+   *
+   * @return array
+   *   All children of collection nodes.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  protected function getResults($collection_ids): array {
+    $originalFileTerm = $this->utils->getTermForUri('http://pcdm.org/use#OriginalFile');
+    $tid = $originalFileTerm->id();
+    $sql = <<<"SQL"
+    select o.field_media_of_target_id as node, m.mid as media, fm.fid, fm.uri as uri
+from media m,
+     media__field_media_file f,
+     media__field_media_of o,
+     file_managed fm,
+     media__field_media_use mu
+where m.mid = f.entity_id
+  and m.mid = o.entity_id
+  and m.mid = mu.entity_id
+  and f.field_media_file_target_id = fm.fid
+  and mu.field_media_use_target_id = $tid
+  and o.field_media_of_target_id in (select entity_id
+                                     from node__field_member_of
+                                     where field_member_of_target_id in (:collection_ids[]));
+SQL;
+    $query = $this->database->query($sql, [':collection_ids[]' => $collection_ids]);
+    return $query->fetchAll();
+  }
+
+  /**
+   * Gets all sub nodes with children.
+   *
+   * @param array $parents
+   *   The parent nodes.
+   *
+   * @return array
+   *   The child nodes.
+   */
+  protected function getCollectors($parents) {
+    $sql = <<<"SQL"
+    select n.nid
+from node n,
+     node__field_member_of me,
+     node__field_model mo
+where n.nid = mo.entity_id
+    and n.nid = me.entity_id
+    and me.field_member_of_target_id in (:parents[])
+    and mo.field_model_target_id in (:terms[]);
+SQL;
+    $query = $this->database->query($sql, [':parents[]' => $parents, ':terms[]' => $this->collection_terms]);
+    return $query->fetchCol();
+  }
+  /**
+   * Processes each media and file.
+   *
+   * @param array $result
+   *   Object with nid, mid, fid and uri.
+   *
+   * @return void
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function processResult($result) {
+    $config = $this->config(static::SETTINGS);
+    $fedora_file = fopen($result->uri, 'r');
+    $temp_name = basename($result->uri);
+    $temp_file = fopen("public://$temp_name", 'w');
+    stream_copy_to_stream($fedora_file, $temp_file);
+    fclose($fedora_file);
+    fclose($temp_file);
+    $s3_uri = str_replace('fedora://', 's3://', $result->uri);
+    $directory = dirname($s3_uri);
+    $filename = basename($s3_uri);
+    $this->s3fs->mkdir($directory, 509);
+    $destination = "$directory/n_{$result->node}-$filename";
+    $new_uri = $this->s3fs->move("public://$temp_name", $destination);
+    $url_base = $config->get('s3_url') . '/';
+    $new_uri = str_replace('s3://', $url_base, $new_uri);
+    $node = $this->entityTypeManager->getStorage('node')->load($result->node);
+    $node->field_s3_archive_link = $new_uri;
+    $node->save();
+    $deletion_candidate = $this->entityTypeManager->getStorage('media')
+      ->load($result->media);
+    $file = $this->entityTypeManager->getStorage('file')->load($result->fid);
+    $file->delete();
+    $deletion_candidate->delete();
+  }
+
+}
---
+++ src/Drush/Commands/S3ArchiveCommands.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Drupal\s3_archive\Drush\Commands;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\islandora\IslandoraUtils;
+use Drush\Attributes as CLI;
+use Drush\Commands\DrushCommands;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\s3fs\S3fsFileSystemD103;
+
+/**
+ * A Drush commandfile.
+ */
+final class S3ArchiveCommands extends DrushCommands {
+  const SETTINGS = 's3_archive.settings';
+
+  /**
+   * Constructs a S3ArchiveCommands object.
+   */
+  public function __construct(
+    private readonly Connection $connection,
+    private readonly IslandoraUtils $utils,
+    private readonly EntityTypeManager $entityTypeManager,
+    private readonly ConfigFactoryInterface $configFactory,
+    private readonly S3fsFileSystemD103 $s3fsFileSystemD103,
+  ) {
+    parent::__construct();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database'),
+      $container->get('islandora.utils'),
+      $container->get('entity_type.manager'),
+      $container->get('config.factory'),
+      $container->get('s3fsfileservice'),
+    );
+  }
+
+  /**
+   * Command description here.
+   */
+  #[CLI\Command(name: 's3_archive:s3ArchiveAll', aliases: ['s3All'])]
+  #[CLI\Usage(name: 's3_archive:command-name foo', description: 'Usage description')]
+  public function s3ArchiveAll() {
+    $originalFileTerm = $this->utils->getTermForUri('http://pcdm.org/use#OriginalFile');
+    $tid = $originalFileTerm->id();
+    $sql = <<<"SQL"
+    select o.field_media_of_target_id as node, m.mid as media, fm.fid, fm.uri as uri
+from media m,
+     media__field_media_file f,
+     media__field_media_of o,
+     file_managed fm,
+     media__field_media_use mu
+where m.mid = f.entity_id
+  and m.mid = o.entity_id
+  and m.mid = mu.entity_id
+  and f.field_media_file_target_id = fm.fid
+  and mu.field_media_use_target_id = $tid
+;
+SQL;
+    $query = $this->connection->query($sql);
+    $results = $query->fetchAll();
+    $todo = count($results);
+    $this->logger()->success("$todo results to be processed");
+    foreach ($results as $result) {
+      $this->processResult($result);
+    }
+  }
+
+  /**
+   * Processes candidate nodes.
+   *
+   * @param $result
+   *
+   * @return void
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function processResult($result) {
+    $config = $this->configFactory->get(static::SETTINGS);
+    $fedora_file = fopen($result->uri, 'r');
+    $temp_name = basename($result->uri);
+    $temp_file = fopen("public://$temp_name", 'w');
+    stream_copy_to_stream($fedora_file, $temp_file);
+    fclose($fedora_file);
+    fclose($temp_file);
+    $s3_uri = str_replace('fedora://', 's3://', $result->uri);
+    $directory = dirname($s3_uri);
+    $filename = basename($s3_uri);
+    $this->s3fsFileSystemD103->mkdir("$directory", 509,true);
+    $destination = "$directory/n_{$result->node}-$filename";
+    $new_uri = $this->s3fsFileSystemD103->move("public://$temp_name", $destination);
+    $url_base = $config->get('s3_url') . '/';
+    $new_uri = str_replace('s3://', $url_base, $new_uri);
+    $node = $this->entityTypeManager->getStorage('node')->load($result->node);
+    $node->field_s3_archive_link = $new_uri;
+    $node->save();
+    $deletion_candidate = $this->entityTypeManager->getStorage('media')
+      ->load($result->media);
+    $file = $this->entityTypeManager->getStorage('file')->load($result->fid);
+    $file->delete();
+    $deletion_candidate->delete();
+  }
+}
